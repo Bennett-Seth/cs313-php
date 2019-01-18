b17870f572efd4ca9db1d4b8f69e31c259489ead
Bennett-Seth <sethbennett08@yahoo.com>
2019-01-18 11:06:09 -0800

    Create index.php

---
+++ web/02_project_homepage/images/index.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE HTML>
+	<html lang="en-us">
+		<head>
+			<meta charset="utf-8">
+			<title>images hold file</title>
+        
+		</head>
+		<body> 
+	
+    <p> Holder text </p>
+        
+		</body>	
+	</html>
