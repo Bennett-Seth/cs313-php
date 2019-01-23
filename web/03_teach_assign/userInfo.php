a68ddd9f018a89491dc06303be657a19ded9a2db
Seth Bennett <sethbennett08@yahoo.com>
2019-01-23 10:34:22 -0800

    trying out a for loop for the array

--- web/03_teach_assign/userInfo.php
+++ web/03_teach_assign/userInfo.php
@@ -6,6 +6,7 @@
    $userComments = htmlspecialchars($_POST['userComments']);
    $userCont = array($_POST['userCont']);
         // How to use as an array?
+    $arrLength = count($cars);
 ?>
 
 <!DOCTYPE HTML>
@@ -22,8 +23,15 @@
             <h2>Comments: <?php echo $userComments; ?>  </h2>
             <h2>Your Continents: 
                 <?php 
-                    foreach ($userCont as $value)
-                        echo "<h2> I have been too: $value </h2><br>";
+                    for($x = 0; $x < arrLength; $x++){
+                        echo $userCont[$x];
+                        echo <br>;
+                        
+                    }
+                
+                //foreach ($userCont as $value){
+                //        echo "<h2> I have been too:           $value </h2><br>";
+                //}
                 ?> 
             
             </h2>
