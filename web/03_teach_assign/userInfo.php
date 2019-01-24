61b1cdce62cb2442bc6dd0dc07600139ccadae4e
Seth Bennett <sethbennett08@yahoo.com>
2019-01-23 20:02:02 -0800

    barely finished the 03 teaching assignment

--- web/03_teach_assign/userInfo.php
+++ web/03_teach_assign/userInfo.php
@@ -1,12 +1,11 @@
 <?php
    $userName = htmlspecialchars($_POST['userName']); 
-   $userEmail = $_POST['userEmail'];
+   $userEmail = htmlspecialchars($_POST['userEmail']);
         // Is this email being input correctly?
-   $userMajor = $_POST['userMajor'];
+   $userMajor = htmlspecialchars($_POST['major']);
    $userComments = htmlspecialchars($_POST['userComments']);
-   $userCont = $_POST['userCont'];
-   // $userContArr = array($userCont);
-        // How to use as an array?
+   
+    $continents = $_POST["continents"];
 ?>
 
 <!DOCTYPE HTML>
@@ -21,21 +20,40 @@
             <h2>Email: <?php echo $userEmail; ?>  </h2>
             <h2>Major: <?php echo $userMajor; ?>  </h2>
             <h2>Comments: <?php echo $userComments; ?>  </h2>
-            <h2>Your Continents: 
-                <?php 
-                    echo $userCont
-             //       <br>
-             //       echo $userContArr
-             //       <br>
-             //       echo $userContArr[0]
-             //       <br>
-             //       echo $userContArr[1];
-                
-                    //foreach ($userContArr as $value){
-                    //    echo "<h2> I have been too: $value </h2><br>";
-                    //    }
-                ?> 
-            
+            <h2>I have been to: 
+                <ul>
+                    <?php 
+                        foreach ($continents as $continent)
+                            {
+                           $continent_clean = htmlspecialchars($continent);
+                            switch ($continent){
+                                case "na":
+                                    echo "<li><p>North America</p></li>";
+                                    break;
+                                case "sa":
+                                    echo "<li><p>South America</p></li>";
+                                    break;   
+                                case "eu":
+                                    echo "<li><p>Europe</p></li>";
+                                    break;
+                                case "as":
+                                    echo "<li><p>Asia</p></li>";
+                                    break;
+                                case "af":
+                                    echo "<li><p>Africa</p></li>";
+                                    break;
+                                case "au":
+                                    echo "<li><p>Australia</p></li>";
+                                    break;
+                                case "an":
+                                    echo "<li><p>Antartica</p></li>";
+                                    break;
+                                default:
+                                    echo "You've got some traveling to do!";
+                                }
+                            }
+                    ?> 
+                </ul>
             </h2>
 		</body>	
 	</html>
