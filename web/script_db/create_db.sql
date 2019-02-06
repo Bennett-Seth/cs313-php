94964807c02a1f06af27d8e9fc520f3c0dd5159d
unknown <seth@sbsebrick.com>
2019-02-06 13:22:02 -0800

    moved into the web file

---
+++ web/script_db/create_db.sql
@@ -0,0 +1,12 @@
+/*CREATE DATABASE */
+CREATE DATABASE Scriptures
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+COMMENT ON DATABASE Scriptures_test
+    IS 'This is a test for my Scriptures Database';
