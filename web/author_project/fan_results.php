e3db4f450e9e9943d69c99ce66180009ece2fe4b
unknown <seth@sbsebrick.com>
2019-02-08 12:45:12 -0800

    echo $fanFirstName?

--- web/author_project/fan_results.php
+++ web/author_project/fan_results.php
@@ -49,7 +49,7 @@
                 
                 <?php 
 					
-                    echo $_SESSION['superFan']['first_name'];
+                    echo $fanFirstName;
                     
                     $lockCheck = $db->query("SELECT * FROM lockout WHERE fans_id = '$fansId';");
                 
