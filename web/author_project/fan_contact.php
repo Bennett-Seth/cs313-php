da6c3897309c71d710f8f53c0cad3e37bb0edd1e
unknown <seth@sbsebrick.com>
2019-02-16 11:04:48 -0800

    basic contact posting info

--- web/author_project/fan_contact.php
+++ web/author_project/fan_contact.php
@@ -51,7 +51,19 @@
                 
                 <?php 
                     
-                 echo "Test: $fanId"; 
+                echo "Test: $fanId"; 
+                
+                foreach ($db->query("SELECT first_name, last_name, email FROM fans WHERE fans_id = '$fanId';") as $row){
+
+                            $firstName = $row['first_name'];
+                                echo "$firstName ";
+                            $lastName = $row['last_name'];
+                                echo "$lastName ";
+                            $email = $row['email'];
+                                echo "$email ";
+
+                            echo "<p>You are: $firstName $lastName </p>";
+                            echo "<p>Your email address is: $email </p>";
                 
                 ?>
                 
