1a1564c2ef36735692515caacd0c8a4cd1977439
unknown <seth@sbsebrick.com>
2019-02-16 10:31:06 -0800

    fixed missing bracket

--- web/author_project/fan_results.php
+++ web/author_project/fan_results.php
@@ -80,13 +80,13 @@
                             $storyTitle = $row['stories_title'];
 
                             echo "<p> You are a first reader for: <b>'$storyTitle'</b>. </p>";
-      /*                        
+                            
                             echo "<p>Do you want to view or change your feedback: Do so <a href='fan_feedback.php'>Here</a> </p>"; 
                              
                             $_SESSION['firstReadId'] = $thisFirstReadId;
 
                             }
-                
+   /*               
                 echo "Test: $_SESSION['firstReadId']";
    
     
