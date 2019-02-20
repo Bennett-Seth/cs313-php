0fddd1cbf180cf853313d71a20c162b0beaba76e
unknown <seth@sbsebrick.com>
2019-02-20 10:34:56 -0800

    printing the contents of $db.

--- web/login_project/functions.php
+++ web/login_project/functions.php
@@ -23,6 +23,8 @@ function signUpUser($username, $hashedPassword){
     $query = 'INSERT INTO users (username, password) VALUES (:username, :password)';
     
     echo "This is my query statement: $query";
+    print_r($db);
+    echo '<br>';
     
     // Create the prepared statement using the db connection
     $statement = $db->prepare($query);
@@ -35,6 +37,8 @@ function signUpUser($username, $hashedPassword){
     
     // Insert the data
     $statement->execute();
+     
+    echo "Insert Successful, we hope.<br>";     
 /*
     // Ask how many rows changed as a result of our insert
     $rowsChanged = $statement->rowCount();
