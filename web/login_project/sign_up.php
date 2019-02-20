e457933f2f3b25fe379ebe6d580f0f288f517b3a
unknown <seth@sbsebrick.com>
2019-02-20 12:24:10 -0800

    working on client-side js, invert inputs

--- web/login_project/sign_up.php
+++ web/login_project/sign_up.php
@@ -18,13 +18,13 @@
                     var pw1 = document.getElementById('password').value;
                     var pw2 = document.getElementById('password_repeat').value;
                     if (pw1 != pw2){
-                        document.getElementById('noMatch').style.color = 'red';
-                        document.getElementById('noMatch').style.visibility = 'visible';
-                        document.getElementById('yesMatch').style.visibility = 'hidden';
-                    } else {
                         document.getElementById('yesMatch').style.color = 'green';
                         document.getElementById('yesMatch').style.visibility = 'visible';
                         document.getElementById('noMatch').style.visibility = 'hidden';
+                    } else {
+                        document.getElementById('noMatch').style.color = 'red';
+                        document.getElementById('noMatch').style.visibility = 'visible';
+                        document.getElementById('yesMatch').style.visibility = 'hidden';
                     }
                 }
             
