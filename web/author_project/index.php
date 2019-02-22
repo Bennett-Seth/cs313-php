dff7ac6741dd021b2984a29a914addc710d83076
unknown <seth@sbsebrick.com>
2019-02-22 14:04:33 -0800

    testing the bare minimum code

--- web/author_project/index.php
+++ web/author_project/index.php
@@ -4,12 +4,14 @@
     session_start();
     //De-Bugging? "heroku logs"
 
+/*
     // Get the database connection file
     require 'library/connect.php';
    
     // Get php functions file
     require 'library/functions.php';
-    
+*/    
+
     $action = filter_input(INPUT_POST, 'action');
         if ($action == NULL){
          $action = filter_input(INPUT_GET, 'action');
@@ -18,6 +20,7 @@
             }
         }
 
+/*
     //check to see if the user is already logged in
     if (isset($_SESSION['loggedin'])){
             $cookieUsername = filter_input(INPUT_COOKIE, 'username', FILTER_SANITIZE_STRING);
@@ -25,15 +28,20 @@
             setcookie('username', '', time() - 3600, '/'); 
             // empty value and old timestamp
         }
+*/
 
 switch ($action){
     case 'home':
         include 'view/fan_reg.php';    
+        echo "This is my first page test.";
         break;
-    
+
+/*    
     default:
          
-        include 'view/fan_reg.php';     break;   
-       
+        include 'view/fan_reg.php';     
+        break;   
+*/       
     }
+
 ?>
