b6a0e1cbb332e2ea5c206cb698d79a6abab0981c
unknown <seth@sbsebrick.com>
2019-02-22 21:59:58 -0800

    did i finally figure out the filepath?

--- web/author_project/common/nav.php
+++ web/author_project/common/nav.php
@@ -2,11 +2,11 @@
     
     <ul class='topnav' id='mainmenu'>
         <li class="active"><a href="https://floating-inlet-17130.herokuapp.com/author_project/view/fan_welcome.php">Welcome</a></li>
-        <li><a href="index.php">Promos</a></li>
-        <li><a href="fan_feedback.php">First Reader</a></li>
-        <li><a href="fan_review.php">Arc Reader</a></li>
-        <li><a href="fan_winner.php">Contest Winner</a></li>
-        <li id="lastLi"><a href="index.php">Account Details</a></li>
+        <li><a href="https://floating-inlet-17130.herokuapp.com/author_project/view/promos.php">Promos</a></li>
+        <li><a href="https://floating-inlet-17130.herokuapp.com/author_project/view/fan_feedback.php">First Reader</a></li>
+        <li><a href="https://floating-inlet-17130.herokuapp.com/author_project/view/fan_review.php">Arc Reader</a></li>
+        <li><a href="https://floating-inlet-17130.herokuapp.com/author_project/view/fan_contest.php">Contest</a></li>
+        <li id="lastLi"><a href="https://floating-inlet-17130.herokuapp.com/author_project/view/fan_contact.php">Account Details</a></li>
     </ul>
         
 </nav>
