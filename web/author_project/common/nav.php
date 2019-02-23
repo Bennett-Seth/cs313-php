c10cc47488467caa70ef7fe35fd395f76b1eb1e0
unknown <seth@sbsebrick.com>
2019-02-22 22:03:13 -0800

    did i finally figure out the filepath?

--- web/author_project/common/nav.php
+++ web/author_project/common/nav.php
@@ -5,7 +5,6 @@
         <li><a href="https://floating-inlet-17130.herokuapp.com/author_project/view/promos.php">Promos</a></li>
         <li><a href="https://floating-inlet-17130.herokuapp.com/author_project/view/fan_feedback.php">First Reader</a></li>
         <li><a href="https://floating-inlet-17130.herokuapp.com/author_project/view/fan_review.php">Arc Reader</a></li>
-        <li><a href="https://floating-inlet-17130.herokuapp.com/author_project/view/fan_contest.php">Contest</a></li>
         <li id="lastLi"><a href="https://floating-inlet-17130.herokuapp.com/author_project/view/fan_contact.php">Account Details</a></li>
     </ul>
         
