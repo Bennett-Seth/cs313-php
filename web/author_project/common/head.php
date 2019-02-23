dcf9bdf529b2c87a932682bf4e2344b3a54cfa9b
unknown <seth@sbsebrick.com>
2019-02-22 21:50:19 -0800

    did i finally figure out the filepath?

--- web/author_project/common/head.php
+++ web/author_project/common/head.php
@@ -1,7 +1,7 @@
 <!-- Here lies your HTML for the head code -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 	    
-<link rel="stylesheet" href="css/normalize.css">
-<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="/app/web/author_project/css/normalize.css">
+<link rel="stylesheet" href="/app/web/author_project/css/main.css">
 
 <script src="scripts/script.js"></script>
