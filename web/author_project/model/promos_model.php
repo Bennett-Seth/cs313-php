153ddbb9ecc4ff687a9dbf3e0174c28036f8ee9b
unknown <seth@sbsebrick.com>
2019-02-22 17:17:50 -0800

    changing string to INT before inserting into database...

--- web/author_project/model/promos_model.php
+++ web/author_project/model/promos_model.php
@@ -1,7 +1,9 @@
 <?php
 
 function callFirstReader($fansId, $db){
-        foreach ($db->query("SELECT first_readers.first_readers_id, first_readers.fans_id, stories.stories_id, stories.stories_title FROM first_readers RIGHT JOIN stories ON first_readers.stories_id = stories.stories_id WHERE first_readers.fans_id = '$fansId';") as $row){
+        $intFansId = (int)$fansId;
+    
+        foreach ($db->query("SELECT first_readers.first_readers_id, first_readers.fans_id, stories.stories_id, stories.stories_title FROM first_readers RIGHT JOIN stories ON first_readers.stories_id = stories.stories_id WHERE first_readers.fans_id = '$intFansId';") as $row){
 
         $thisFirstReadId = $row['first_readers_id'];
         $thisFansId = $row['fans_id'];
