aba0c72d0a53a64cecdab8d4dfd4be32e41ea097
unknown <seth@sbsebrick.com>
2019-02-23 04:05:33 -0800

    fixed missing brackets in fan page

--- web/author_project/view/fan_welcome.php
+++ web/author_project/view/fan_welcome.php
@@ -41,7 +41,7 @@
                     //Post First Reader Details
                     if (isset($_SESSION["firstReadMsg"])){
                             echo $_SESSION["firstReadMsg"];
-                            echo "To leave or update your feedback, click <a href='https://floating-inlet-17130.herokuapp.com/author_project/view/fan_feedback.php'>Here</a>"
+                            echo "To leave or update your feedback, click <a href='https://floating-inlet-17130.herokuapp.com/author_project/view/fan_feedback.php'>Here</a>";
                         } 
                 ?>
                 
@@ -49,8 +49,8 @@
                     //Post First Reader Details
                     if (isset($_SESSION["arcReadMsg"])){
                             $_SESSION["arcReadMsg"];
-                            echo "To leave or update your review, click <a href='https://floating-inlet-17130.herokuapp.com/author_project/view/fan_review.php'>Here</a>"
-                            echo "To update your mailing address, click <a href='https://floating-inlet-17130.herokuapp.com/author_project/view/fan_review.php'>Here</a>"
+                            echo "To leave or update your review, click <a href='https://floating-inlet-17130.herokuapp.com/author_project/view/fan_review.php'>Here</a>";
+                            echo "To update your mailing address, click <a href='https://floating-inlet-17130.herokuapp.com/author_project/view/fan_review.php'>Here</a>";
                         }
                 ?>
                 
