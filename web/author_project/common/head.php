423b6b9b0102737ade56a338ddd6f98e949bc060
unknown <seth@sbsebrick.com>
2019-02-26 10:45:02 -0800

    adding a fantasy font to the h tags

--- web/author_project/common/head.php
+++ web/author_project/common/head.php
@@ -3,6 +3,7 @@
 	    
 <link rel="stylesheet" href="https://floating-inlet-17130.herokuapp.com/author_project/css/normalize.css">
 <link rel="stylesheet" href="https://floating-inlet-17130.herokuapp.com/author_project/css/main.css">
+<link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
 
 <script src="scripts/script.js"></script>
 <script src="scripts/currentMenu.js"></script>
