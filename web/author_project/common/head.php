dcb2e8c5f53ac1ef4aca986156e668aa10d96fed
unknown <seth@sbsebrick.com>
2019-02-26 10:17:04 -0800

    tweaking the header reference to the css files.

--- web/author_project/common/head.php
+++ web/author_project/common/head.php
@@ -1,7 +1,8 @@
 <!-- Here lies your HTML for the head code -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 	    
-<link rel="stylesheet" href="/app/web/author_project/css/normalize.css">
-<link rel="stylesheet" href="/app/web/author_project/css/main.css">
+<link rel="stylesheet" href="https://floating-inlet-17130.herokuapp.com/author_project/css/normalize.css">
+<link rel="stylesheet" href="https://floating-inlet-17130.herokuapp.com/author_project/css/main.css">
 
 <script src="scripts/script.js"></script>
+<script src="scripts/currentMenu.js"></script>
