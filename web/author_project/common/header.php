75bdbcb1f0a2a94cd2f8160f9a76f47d93c5d64f
unknown <seth@sbsebrick.com>
2019-02-26 10:37:37 -0800

    added a KT banner

--- web/author_project/common/header.php
+++ web/author_project/common/header.php
@@ -1,3 +1,4 @@
 <header>
+    <img id="banner" src="https://floating-inlet-17130.herokuapp.com/author_project/css/img/KT_banner.jpg" alt="Assassin's Rising Banner Image">
     <h1>Welcome, Super Fan!</h1>
 </header>
